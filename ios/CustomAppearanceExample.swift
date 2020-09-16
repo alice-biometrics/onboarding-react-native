ec9cc95640f38fbb7eec64c2f59df10d1e57d89f
Adrián García <adriangarzia90@gmail.com>
2020-09-16 02:39:55 +0200

    Added example to change android styles

---
+++ ios/CustomAppearanceExample.swift
@@ -0,0 +1,9 @@
+//
+//  CustomAppearanceExample.swift
+//  AppOnboardingSample
+//
+//  Created by Adrian Garcia Garcia on 16/09/2020.
+//  Copyright © 2020 Facebook. All rights reserved.
+//
+
+import Foundation
